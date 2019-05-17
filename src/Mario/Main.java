eb2f64241c55296b921aa0c9a9d69511e13e1b9a
Lucas Wargha <war18001@byui.edu>
2019-05-17 09:55:46 -0600

    Hi from Lucas

--- src/Mario/Main.java
+++ src/Mario/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Its me Mario, Hello Lucas, Sean and Nakai");
+        System.out.println("Hello, it's me Lucas! :)");
     }
 }
