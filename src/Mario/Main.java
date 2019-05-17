ec45486894c2648f2a19dc313421283754767f92
“Mario <das16005.byui.edu>
2019-05-17 09:49:21 -0600

    Hello for you guys

---
+++ src/Mario/Main.java
@@ -0,0 +1,9 @@
+package Mario;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Its me Mario, Hello Lucas, Sean and Nakai");
+    }
+}
